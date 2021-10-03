33f42dd9f252fefd39e97d401004ccb8cdbb6a99
Martin Cao <martin@gmail.com>
2021-10-03 15:58:38 +1100

    Import the entire helpers object instead of using named exports

--- src/scripts/bce.ts
+++ src/scripts/bce.ts
@@ -4,12 +4,13 @@ export {}
 const fetch = require('node-fetch')
 const dotenv = require('dotenv')
 const _ = require('lodash')
-const {importJson, calculateStartTime, calculateEndTime, ftxSign} = require('../modules/helpers')
+// const {importJson, calculateStartTime, calculateEndTime, ftxSign} = require('../modules/helpers')
+const helpers = require('../modules/helpers')
 
 // Load environment variables
 dotenv.config()
 
-const params = importJson('params/default.json')
+const params = helpers.importJson('params/default.json')
 
 // Base URL
 const method = 'GET'
@@ -18,15 +19,15 @@ const baseUrl = 'https://ftx.com'
 // GET Parameters
 const market = params.market
 const resolutionSeconds = params.resolution
-const startTime = calculateStartTime(params.startTime)
+const startTime = helpers.calculateStartTime(params.startTime)
 const startTimeSeconds = startTime.getTime()/1000
 const startTimeFormatted = startTime.toLocaleString('en-AU')
-const endTime = calculateEndTime(params.startTime, resolutionSeconds, params.numCandles)
+const endTime = helpers.calculateEndTime(params.startTime, resolutionSeconds, params.numCandles)
 const endTimeSeconds = endTime.getTime()/1000
 const requestPath = `/api/markets/${market}/candles?resolution=${resolutionSeconds}&start_time=${startTimeSeconds}&end_time=${endTimeSeconds}`
 
 const timestamp = Date.now()
-const signature = ftxSign(process.env.API_SECRET, timestamp, method, requestPath)
+const signature = helpers.ftxSign(process.env.API_SECRET, timestamp, method, requestPath)
 
 const headers = {
   "FTX-KEY": process.env.API_KEY,
