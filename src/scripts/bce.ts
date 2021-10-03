669e3ba8e8d8ace1aa62c0a56bdf0c29ae02fc5f
Martin Cao <martin@gmail.com>
2021-10-03 17:13:46 +1100

    Implement working prototype for fetching multiple markets in parallel

--- src/scripts/bce.ts
+++ src/scripts/bce.ts
@@ -9,13 +9,14 @@ const helpers = require('../modules/helpers')
 // Load environment variables
 dotenv.config()
 
-const params = helpers.importJson('params/default.json')
+const params = helpers.importJson('params/default.json');
 
-helpers.FTXFetch(params, process.env.API_KEY, process.env.API_SECRET)
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json)
-  })
-  .catch(err => {
-    console.error(err)
+Promise.all(params.map(async market => {
+  const response = await helpers.FTXFetch(market, process.env.API_KEY, process.env.API_SECRET)
+  return response.json()
+}))
+  .then(results => {
+    results.forEach(result => {
+      console.log(result)
+    })
   })
