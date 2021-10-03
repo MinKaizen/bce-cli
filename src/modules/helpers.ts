86736d799774c28056b4a90dd11cc56ef554168b
Martin Cao <martin@gmail.com>
2021-10-03 12:32:44 +1100

    Add function importJson to module helpers

--- src/modules/helpers.ts
+++ src/modules/helpers.ts
@@ -1,6 +1,29 @@
 'use strict'
 export {}
 
+const fs = require('fs')
+
+let helpers: any = {}
+
+/**
+ * Import a json file as an object
+ * 
+ * Pre Conditions:
+ * - path is a string
+ * - path points to a valid json file
+ */
+const importJson = (path: String, relativeToRoot: Boolean = true): Object => {
+  const missingDotSlash = relativeToRoot && !path.startsWith('./')
+  const missingFileExtension = !path.endsWith('.json')
+  let finalPath = path
+  finalPath = missingDotSlash ? './' + finalPath : finalPath
+  finalPath = missingFileExtension ? finalPath + '.json' : finalPath
+  const raw = fs.readFileSync(finalPath)
+  const parsed = JSON.parse(raw)
+  return parsed
+}
+helpers.importJson = importJson
+
 /**
  * Calculate end time in seconds based on a start time, resolution in seconds and number of candles
  * 
@@ -14,5 +37,7 @@ export {}
 const calculateEndTime = (startTime: number, resolutionSeconds: number, numCandles: number): number => {
   return startTime + (numCandles - 1) * resolutionSeconds
 }
+helpers.calculateEndTime = calculateEndTime
 
-module.exports['calculateEndTime'] = calculateEndTime
+// Default export
+module.exports = helpers
