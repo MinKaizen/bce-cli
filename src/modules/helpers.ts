2c1120bbde349e963ff7d7acd2798bf0b7a6e645
Martin Cao <martin@gmail.com>
2021-10-03 13:29:43 +1100

    Add helper function calculateStartTime

--- src/modules/helpers.ts
+++ src/modules/helpers.ts
@@ -25,6 +25,30 @@ const importJson = (path: String, relativeToRoot: Boolean = true): Object => {
 helpers.importJson = importJson
 
 /**
+ * Calculate Start time in seconds since epoch given a 24 hour time string
+ * @param timeStringUTC - string representing the UTC time of day in 24 hour format. e.g. '10:00:00' = 10AM
+ * @returns @number - The start time in seconds since epoch
+ * 
+ * Pre Conditions:
+ * - timeStringUTC is a a valid 24 hour string in HH:mm:ss format
+ */
+const calculateStartTime = (timeStringUTC: String): number => {
+  const targetTime = new Date(`1970-01-01T${timeStringUTC}+00:00`)
+  const targetHours = targetTime.getUTCHours()
+  const targetMinutes = targetTime.getUTCMinutes()
+  const targetSeconds = targetTime.getUTCSeconds()
+
+  let startTime = new Date()
+  startTime.setUTCHours(targetHours)
+  startTime.setUTCMinutes(targetMinutes)
+  startTime.setUTCSeconds(targetSeconds)
+  startTime.setUTCMilliseconds(0)
+  const startTimeSeconds = startTime.getTime()/1000
+  return startTimeSeconds
+}
+helpers.calculateStartTime = calculateStartTime
+
+/**
  * Calculate end time in seconds based on a start time, resolution in seconds and number of candles
  * 
  * Pre Conditions:
