395d497cbf1d9871d67c9cde0b4838047b8dc649
Martin Cao <martin.g.cao@gmail.com>
2021-12-23 00:32:21 +1100

    Refactor bce script to use helper function candlesToCSV

--- src/scripts/bce.ts
+++ src/scripts/bce.ts
@@ -4,7 +4,7 @@ export {}
 
 const dotenv = require("dotenv")
 const FTXClient = require("../modules/FTXClient")
-const { importJson, candleToCSV } = require("../modules/helpers")
+const { importJson, candlesToCSV } = require("../modules/helpers")
 
 // Load environment variables
 dotenv.config()
@@ -18,16 +18,8 @@ Promise.all(
   markets.map((market) => {
     return ftx.fetch(market)
   })
-)
-  .then((candles) => {
-    const headers = ["market", "open", "high", "low", "close"]
-    const rows = candles.map((candle) => {
-      return candleToCSV(candle, headers)
-    })
-    const csv = [headers.join(","), ...rows].join("\n")
-    return csv
-  })
-  .then((csv) => {
-    console.log(csv)
-    // console.log(FTXResultsToCSV(results))
-  })
+).then((candles) => {
+  const headers = ["market", "open", "high", "low", "close"]
+  const csv = candlesToCSV(candles, headers)
+  console.log(csv)
+})
