1d17b6397b63656ec569ddcf6bea55a15c415493
Martin Cao <martin.g.cao@gmail.com>
2021-12-23 00:54:41 +1100

    Include binance markets in bce script

--- src/scripts/bce.ts
+++ src/scripts/bce.ts
@@ -4,6 +4,7 @@ export {}
 
 const dotenv = require("dotenv")
 const FTXClient = require("../modules/FTXClient")
+const BinanceClient = require("../modules/BinanceClient")
 const { importJson, candlesToCSV } = require("../modules/helpers")
 
 // Load environment variables
@@ -11,14 +12,46 @@ dotenv.config()
 
 const options = importJson("/config/default.json")
 
-var ftx = new FTXClient(process.env.API_KEY, process.env.API_SECRET, options)
-const markets = ["btc-perp", "eth-perp", "sol-perp", "ftm-perp"]
+const ftxOptions = {
+  resolutionMinutes: options.resolutionMinutes,
+  startTimeUTC: options.startTimeUTC,
+}
+const ftx = new FTXClient(
+  process.env.API_KEY,
+  process.env.API_SECRET,
+  ftxOptions
+)
+const binanceOptions = {
+  resolutionMinutes: options.resolutionMinutes,
+  startTimeUTC: options.startTimeUTC,
+}
+const binance = new BinanceClient(
+  process.env.BINANCE_API_KEY,
+  process.env.BINANCE_API_SECRET,
+  binanceOptions
+)
 
-Promise.all(
-  markets.map((market) => {
-    return ftx.fetch(market)
-  })
-).then((candles) => {
+interface Candle {
+  market: string
+  open: number
+  high: number
+  low: number
+  close: number
+}
+
+const clientMap = {
+  binance: binance,
+  ftx: ftx,
+}
+
+const candles: Array<Candle> = options.markets.map((clientMarket) => {
+  const clientName = clientMarket.split(":")[0]
+  const market = clientMarket.split(":")[1]
+  const client = clientMap[clientName]
+  return client.fetch(market)
+})
+
+Promise.all(candles).then((candles) => {
   const headers = ["market", "open", "high", "low", "close"]
   const csv = candlesToCSV(candles, headers)
   console.log(csv)
