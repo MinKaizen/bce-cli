38e19f50dc8a46d9cd68620d9ab30fbe3c57a2a5
Martin Cao <martin.g.cao@gmail.com>
2021-12-22 23:23:16 +1100

    Refactor fetch function

--- src/modules/BinanceClient.ts
+++ src/modules/BinanceClient.ts
@@ -30,34 +30,40 @@ class BinanceClient {
   }
 
   async fetch(market: string) {
-    const resolutionSeconds = this.options.resolutionMinutes * 60
-    const startTime = helpers.calculateStartTime(this.options.startTimeUTC)
-    const startTimeMilliseconds = startTime.getTime()
-    const numCandles = 1
-    const endTime = helpers.calculateEndTime(
-      this.options.startTimeUTC,
-      resolutionSeconds,
-      numCandles
-    )
-    const endTimeMilliseconds = endTime.getTime()
-    const interval = this.options.resolutionMinutes.toString() + "m"
-    const options = {
-      startTime: startTimeMilliseconds,
-      endTime: endTimeMilliseconds,
-      limit: numCandles,
-    }
+    const interval = this.makeInterval(this.options.resolutionMinutes)
+    const options = this.makeOptions(this.options)
     const res = await this.client.klines(market, interval, options)
-    const candle = await {
-      market: market,
-      open: res.data[0][1],
-      high: res.data[0][2],
-      low: res.data[0][3],
-      close: res.data[0][4],
-    }
+    const candle = this.makeCandle(market, res)
     return candle
   }
 
-  generateOptions() {}
+  makeOptions(interfaceOptions: InterfaceOptions): VendorOptions {
+    const startTime = helpers.calculateStartTime(interfaceOptions.startTimeUTC)
+    const startTimeMilliseconds = startTime.getTime()
+    const endTimeMilliseconds =
+      startTimeMilliseconds + interfaceOptions.resolutionMinutes * 60 * 1000
+    const options = {
+      startTime: startTimeMilliseconds,
+      endTime: endTimeMilliseconds,
+      limit: 1,
+    }
+    return options
+  }
+
+  makeInterval(resolutionMinutes: number) {
+    return resolutionMinutes.toString() + "m"
+  }
+
+  makeCandle(market: string, response) {
+    const candle = {
+      market: market,
+      open: response.data[0][1],
+      high: response.data[0][2],
+      low: response.data[0][3],
+      close: response.data[0][4],
+    }
+    return candle
+  }
 }
 
 module.exports = BinanceClient
